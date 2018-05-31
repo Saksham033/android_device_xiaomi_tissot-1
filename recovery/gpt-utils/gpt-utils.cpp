a6bb214bb209e4c1ce2e7133a7a0203484e9e2c3
Bauuuuu <abansode@outlook.com>
2018-05-31 19:51:52 +0530

    Hardcode the path
    Change-Id: Ib27b8f1ed438bda61ded7cdce60d87afa71ecbc9

--- recovery/gpt-utils/gpt-utils.cpp
+++ recovery/gpt-utils/gpt-utils.cpp
@@ -65,10 +65,10 @@
 /* list the names of the backed-up partitions to be swapped */
 /* extension used for the backup partitions - tzbak, abootbak, etc. */
 #define BAK_PTN_NAME_EXT    "bak"
-#define XBL_PRIMARY         "/dev/block/bootdevice/by-name/xbl"
-#define XBL_BACKUP          "/dev/block/bootdevice/by-name/xblbak"
-#define XBL_AB_PRIMARY      "/dev/block/bootdevice/by-name/xbl_a"
-#define XBL_AB_SECONDARY    "/dev/block/bootdevice/by-name/xbl_b"
+#define XBL_PRIMARY         "/dev/block/platform/soc/7824900.sdhci/by-name/xbl"
+#define XBL_BACKUP          "/dev/block/platform/soc/7824900.sdhci/by-name/xblbak"
+#define XBL_AB_PRIMARY      "/dev/block/platform/soc/7824900.sdhci/by-name/xbl_a"
+#define XBL_AB_SECONDARY    "/dev/block/platform/soc/7824900.sdhci/by-name/xbl_b"
 /* GPT defines */
 #define MAX_LUNS                    26
 //Size of the buffer that needs to be passed to the UFS ioctl
@@ -1052,7 +1052,7 @@ int prepare_boot_update(enum boot_update_stage stage)
 
 //Given a parttion name(eg: rpm) get the path to the block device that
 //represents the GPT disk the partition resides on. In the case of emmc it
-//would be the default emmc dev(/dev/mmcblk0). In the case of UFS we look
+//would be the default emmc dev(/dev/block/mmcblk0). In the case of UFS we look
 //through the /dev/block/bootdevice/by-name/ tree for partname, and resolve
 //the path to the LUN from there.
 static int get_dev_path_from_partition_name(const char *partname,
@@ -1081,7 +1081,7 @@ static int get_dev_path_from_partition_name(const char *partname,
                         buf[PATH_TRUNCATE_LOC] = '\0';
                 }
         } else {
-                snprintf(buf, buflen, "/dev/mmcblk0");
+                snprintf(buf, buflen, "/dev/block/mmcblk0");
         }
         return 0;
 
